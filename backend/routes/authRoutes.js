7720cfd1966105de5c3533a1bf6c5467cbd79e41
Sahil <sahilkv.5743@gmail.com>
2025-05-28 11:22:40 +0530

    fix backend connecting  issue

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login } = require("../controllers/AuthController");
+// FIX: Use correct case for controller import
+const { signup, login } = require("../controllers/authController");
 
 router.post("/signup", signup);
 router.post("/login", login);
