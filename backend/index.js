abf9aa745e1f462221515cd4a5bcfc48db02fc4d
Sahil <sahilkv.5743@gmail.com>
2025-05-28 11:35:33 +0530

    fix backend connecting  issue

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,10 @@ app.use("/api/users", userRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch((err) => console.error("Local MongoDB error:", err));
 
